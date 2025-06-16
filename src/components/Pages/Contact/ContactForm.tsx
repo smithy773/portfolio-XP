6591f4c25b716a38abae7b571f66bdd9f802dd9f
Viktor Kolev <viktorkolev0707@gmail.com>
2025-06-16 19:47:01 +0300

    Style ContactForm

--- src/components/Pages/Contact/ContactForm.tsx
+++ src/components/Pages/Contact/ContactForm.tsx
@@ -33,14 +33,39 @@ export default function ContactForm() {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" id="name" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" id="email" />
-      <label>Message</label>
-      <textarea name="message" id="message" />
-      <input type="submit" value="Send" />
+    <form
+      className="flex flex-col items-center gap-8 border-2 rounded-md border-cyan-500 p-8 m-auto"
+      ref={form}
+      onSubmit={sendEmail}
+    >
+      <div className="grid sm:grid-cols-2 grid-cols-1 sm:grid-rows-4 grid-rows-5 items-center gap-5">
+        <div className="form-div">
+          <label>Name</label>
+          <input className="backgr-p" type="text" id="name" name="user_name" />
+        </div>
+        <div className="form-div">
+          <label>Email</label>
+          <input
+            className="backgr-p"
+            type="email"
+            name="user_email"
+            id="email"
+          />
+        </div>
+        <div className="text-lg form-div h-full w-full sm:col-start-1 sm:col-end-3 sm:row-start-2 row-start-3 row-end-6">
+          <label>Message</label>
+          <textarea
+            className="backgr-p w-full h-full"
+            name="message"
+            id="message"
+          />
+        </div>
+      </div>
+      <input
+        className="outline-cyan-500 outline-3 rounded px-3 py-1 hover:bg-cyan-500 transition-all hover:cursor-pointer text-3xl"
+        type="submit"
+        value="Send"
+      />
     </form>
   );
 }
